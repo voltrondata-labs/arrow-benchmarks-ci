786e8f8f0e27eb2d74df9bf05d42d94ae5ac4cbc
Elena Henderson <elena@e.local>
2021-11-18 11:08:22 -0800

    Cleanup Dockerfile 1

--- buildkite/benchmark-test/run.sh
+++ buildkite/benchmark-test/run.sh
@@ -6,9 +6,9 @@ export ARROW_REPO="https://github.com/apache/arrow.git" \
     BENCHMARKABLE="641554b0bcce587549bfcfd0cde3cb4bc23054aa" \
     PYTHON_VERSION="3.8" \
     BENCHMARKS_DATA_DIR="/test" \
-    FILTERS="{\"lang\":\"Java\"}" \
-    RUN_ID="test1234567" \
-    RUN_NAME="rtest1" \
+    FILTERS="{}" \
+    RUN_ID="testable-builds" \
+    RUN_NAME="testable-builds" \
     MACHINE="docker-test" \
     CONBENCH_URL="https://conbench.ursa.dev"
 
