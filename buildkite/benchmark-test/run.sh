2cd43a86226fe02caccd77f915c0f2ffbb358f0f
Elena Henderson <elena@e.local>
2021-11-17 23:06:05 -0800

    Change Dockerfile 1

---
+++ buildkite/benchmark-test/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+eval "$(command '/root/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+export ARROW_REPO="https://github.com/apache/arrow.git" \
+    BENCHMARKABLE_TYPE="arrow-commit" \
+    BENCHMARKABLE="641554b0bcce587549bfcfd0cde3cb4bc23054aa" \
+    PYTHON_VERSION="3.8" \
+    BENCHMARKS_DATA_DIR="/test" \
+    FILTERS="{\"lang\": \"R\"}" \
+    RUN_ID="test1234567" \
+    RUN_NAME="rtest1" \
+    MACHINE="docker-test" \
+    CONBENCH_URL="https://conbench.ursa.dev"
+
+source buildkite/benchmark/utils.sh create_conda_env_with_arrow
+source buildkite/benchmark/utils.sh install_conbench
+python -m buildkite.benchmark.run_benchmark_groups
