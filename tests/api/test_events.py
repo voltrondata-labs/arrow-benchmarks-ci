cb7d0e7e824efea21347b66de6fdbffcb9cd7125
Elena Henderson <elena@e.local>
2021-12-02 08:46:05 -0800

    WIP

--- tests/api/test_events.py
+++ tests/api/test_events.py
@@ -107,7 +107,7 @@ pull_comments_with_expected_machine_run_filters_and_skip_reason = {
         ),
         "new-machine": (
             {"lang": "Python", "name": "file-write"},
-            "Only ['lang', 'command'] filters are supported on new-machine",
+            "Only ['C++', 'Java'] filters are supported on new-machine",
         ),
     },
     "@ursabot please benchmark    name=file-write  lang=Python ": {
