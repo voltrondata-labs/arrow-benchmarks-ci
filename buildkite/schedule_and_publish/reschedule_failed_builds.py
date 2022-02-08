94f544cd392fc486bee7c0f9bf41bdad5dacb46e
Elena Henderson <elena@e.local>
2022-02-08 13:25:13 -0800

    WIP

--- buildkite/schedule_and_publish/reschedule_failed_builds.py
+++ buildkite/schedule_and_publish/reschedule_failed_builds.py
@@ -8,10 +8,14 @@ from buildkite.schedule_and_publish.create_benchmark_builds import (
 from models.run import Run
 
 
-def rerun_failed_builds():
-    machines = os.getenv("MACHINES_WITH_FAILED_BUILDS").split("/n")
+def reschedule_failed_builds():
+    machines = os.getenv("MACHINES_WITH_FAILED_BUILDS")
     hours = int(os.getenv("HOURS_WITH_FAILED_BUILDS"))
     runs = Run.all(status="failed")
+
+    print(
+        f"Rescheduling failed builds created in the last {hours} hours for these machines: {machines}"
+    )
 
     runs = [
         r
@@ -33,6 +37,9 @@ def rerun_failed_builds():
         run.machine_info = None
         run.conda_packages = None
         run.save()
+        print(
+            f"Rescheduled failed build for {run.benchmarkable_id} on {run.machine_name}"
+        )
 
     # Set finished_at to None for all notifications associated with failed builds
     for run in runs:
