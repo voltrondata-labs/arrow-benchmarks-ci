1b8a1bb1094f790e97fbeb4beccaf649101c34e2
Elena Henderson <elena@e.local>
2022-04-13 18:43:21 -0700

    WIP

--- models/benchmarkable.py
+++ models/benchmarkable.py
@@ -244,6 +244,7 @@ class Benchmarkable(Base, BaseMixin):
     def has_high_level_of_regressions(self):
         for run in self.runs_with_buildkite_builds_and_publishable_benchmark_results:
             results = self.get_conbench_compare_results(run.machine)
+            print("results")
 
             # Filter results by Python and R benchmarks
             results = [r for r in results if r["language"] in ["Python", "R"]]
