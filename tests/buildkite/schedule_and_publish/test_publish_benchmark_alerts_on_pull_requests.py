3eb53dac59797bb962ef039fad170a9a094f571c
Elena Henderson <elena@e.local>
2022-04-13 18:36:35 -0700

    Try this test

--- tests/buildkite/schedule_and_publish/test_publish_benchmark_alerts_on_pull_requests.py
+++ tests/buildkite/schedule_and_publish/test_publish_benchmark_alerts_on_pull_requests.py
@@ -1,5 +1,6 @@
 import sqlalchemy as s
 
+from buildkite.schedule_and_publish.get_commits import get_commits
 from buildkite.schedule_and_publish.publish_benchmark_alerts_on_pull_requests import (
     publish_benchmark_alerts_on_pull_requests,
 )
@@ -13,9 +14,10 @@ from tests.helpers import (
 
 
 def test_publish_benchmark_alerts_on_pull_requests(client):
-    make_github_webhook_event_for_comment(
-        client, comment_body="@ursabot please benchmark"
-    )
+    get_commits()
+    contender = Benchmarkable.get("f2f663be0a87e13c9cd5403dea51379deb4cf04d")
+    baseline = Benchmarkable.get("c6fdeaf9fb85622242963dc28660e9592088986c")
+
     publish_benchmark_alerts_on_pull_requests()
     # Verify new pull comment was not created since no run finished
     assert (
@@ -24,12 +26,17 @@ def test_publish_benchmark_alerts_on_pull_requests(client):
     )
 
     i = 1
-    for run in Run.all():
-        run.id = str(i)
+    for run in contender.runs:
+        run.id = str(i) + "high"
         run.finished_at = s.sql.func.now()
         run.status = "finished"
         run.save()
         i += 1
+
+    for run in baseline.runs:
+        run.finished_at = s.sql.func.now()
+        run.status = "finished"
+        run.save()
 
     publish_benchmark_alerts_on_pull_requests()
     # Verify pull comment was updated
