a2959ea449d306f4a1f05e0ac9561512e5313c07
Elena Henderson <elena@e.local>
2022-04-13 18:25:11 -0700

    Try this test

--- tests/buildkite/schedule_and_publish/test_publish_benchmark_alerts_on_pull_requests.py
+++ tests/buildkite/schedule_and_publish/test_publish_benchmark_alerts_on_pull_requests.py
@@ -1,29 +1,15 @@
 import sqlalchemy as s
 
-from buildkite.schedule_and_publish.get_commits import get_commits
 from buildkite.schedule_and_publish.publish_benchmark_alerts_on_pull_requests import (
     publish_benchmark_alerts_on_pull_requests,
 )
 from models.benchmarkable import Benchmarkable
-from models.machine import Machine
 from models.run import Run
 from tests.helpers import (
-    machine_configs,
     make_github_webhook_event_for_comment,
     outbound_requests,
-    test_baseline_benchmarkable_id,
     test_benchmarkable_id,
 )
-
-machines = list(machine_configs.keys())
-finished_status = "Finished :arrow_down:33.33% :arrow_up:33.33%"
-failed_status = "Failed :arrow_down:33.33% :arrow_up:33.33%"
-scheduled_status_with_warning = "Scheduled :warning: ursa-i9-9960x is offline."
-skipped_status = "Skipped :warning: Only ['C++', 'Java'] langs are supported on ursa-thinkcentre-m75q"
-
-support_benchmarks_info = """Supported benchmarks:
-ursa-i9-9960x: Supported benchmark langs: Python, R, JavaScript
-ursa-thinkcentre-m75q: Supported benchmark langs: C++, Java"""
 
 
 def test_publish_benchmark_alerts_on_pull_requests(client):
