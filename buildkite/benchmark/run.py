f00fa7e70f063f90644a117f5e12b6a521bf4a81
Elena Henderson <elena@e.local>
2022-04-26 14:00:12 -0700

    WIP

--- buildkite/benchmark/run.py
+++ buildkite/benchmark/run.py
@@ -210,7 +210,13 @@ class Run:
             "url_for_benchmark_groups_list_json"
         ]
         self.benchmarkable_type = os.getenv("BENCHMARKABLE_TYPE")
-        self.filters = json.loads(os.getenv("FILTERS", "{}"))
+        self.filters = {
+                    "langs": {
+                        "Python": {"names": ["dataset-read", "dataset-select"]},
+                        "C++": {"names": ["cpp-micro"]},
+                        "R": {"names": ["tpch"]},
+                    }
+                }
         self.setup_commands_for_lang_benchmarks = repo_params[
             "setup_commands_for_lang_benchmarks"
         ]
