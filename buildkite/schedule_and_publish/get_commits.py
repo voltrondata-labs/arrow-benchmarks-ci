6536909fcf7770df3dc8d9f0e6ca915ca3039711
ElenaHenderson <elenavhenderson@gmail.com>
2022-07-12 14:29:57 -0700

    Fix get_originating_pull_number() in get_commits() (#78)

--- buildkite/schedule_and_publish/get_commits.py
+++ buildkite/schedule_and_publish/get_commits.py
@@ -1,35 +1,27 @@
-import re
-
 from config import Config
 from models.benchmarkable import Benchmarkable
 from integrations.github import Github
 from logger import log
 
-# apache/arrow commits into master branch have messages
-# that contain originating pull request number in phrases like "\n\nCloses #10973 from ..."
-pattern = "\nCloses #(.*) from"
-compiled_pattern = re.compile(pattern)
 
-
-def get_originating_pull_number(commit_dict):
+def get_originating_pull_number(github, commit_dict):
     try:
-        return (
-            compiled_pattern.search(commit_dict["commit"]["message"]).group(1).strip()
-        )
+        return github.get_pulls(commit_dict["sha"])[0]["number"]
     except Exception as e:
         log.error(
             f"Unable to get originating pull number for commit = {commit_dict['sha']} "
-            f"from message = {commit_dict['commit']['message']}"
+            f"with message = {commit_dict['commit']['message']}"
         )
         log.exception(e)
 
 
 def get_commits_for_repo(repo, benchmarkable_type):
-    commit_dicts = Github(repo).get_commits()
+    github = Github(repo)
+    commit_dicts = github.get_commits()
     commit_dicts.reverse()
 
     for commit_dict in commit_dicts:
-        pull_number = get_originating_pull_number(commit_dict)
+        pull_number = get_originating_pull_number(github, commit_dict)
 
         if len(commit_dict["parents"]) > 0:
             baseline_id = commit_dict["parents"][-1]["sha"]
