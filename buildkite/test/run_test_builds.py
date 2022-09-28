3c33bc041446efac5680aa0b8b5cb4643c36bcd4
Elena Henderson <elena@e.local>
2022-09-28 10:48:28 -0700

    WIP

--- buildkite/test/run_test_builds.py
+++ buildkite/test/run_test_builds.py
@@ -1,9 +1,12 @@
 import os
 
+from integrations.buildkite import buildkite
+
 
 def run_test_builds():
     machines = os.getenv("MACHINES_WITH_TEST_BUILDS")
     print(machines)
+    print(buildkite.get_scheduled_builds("arrow-bci-benchmark-on-ursa-i9-9960x"))
 
 
 run_test_builds()
