c2f645777ac1e4eaea2ff51830455099ca0405d1
Austin Dickey <austin3spammy@gmail.com>
2023-07-21 14:31:21 -0500

    benchalerts_run: handle Nones

--- models/benchalerts_run.py
+++ models/benchalerts_run.py
@@ -210,12 +210,10 @@ class ArrowAlerter(Alerter):
         result_info looks like the response from this endpoint:
         https://conbench.ursa.dev/api/redoc#tag/Comparisons/paths/~1api~1compare~1benchmark-results~1%7Bcompare_ids%7D~1/get
         """
-        return (
-            result_info
-            and "contender" in result_info
-            and "language" in result_info["contender"]
-            and result_info["contender"]["language"] not in ["Python", "R"]
-        )
+        contender = result_info["contender"]
+        if not contender:
+            return False
+        return contender["language"] not in ["Python", "R"]
 
     def _separate_known_unstable_benchmarks(
         self,
