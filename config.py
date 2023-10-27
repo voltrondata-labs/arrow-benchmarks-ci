d86b7cbfe62eec357c854f396a1ca225d66701f5
Austin Dickey <austin3spammy@gmail.com>
2023-10-27 13:22:12 -0500

    update machine config

--- config.py
+++ config.py
@@ -98,7 +98,8 @@ class Config:
                                 "tpch",
                             ]
                         },
-                        "JavaScript": {"names": ["js-micro"]},
+                        # Deactivating until https://github.com/apache/arrow/pull/37668 is merged
+                        # "JavaScript": {"names": ["js-micro"]},
                     },
                     "commit_message_skip_strings": [
                         "[C#]",
@@ -135,21 +136,21 @@ class Config:
             "publish_benchmark_results": True,
             "max_builds": 1,
         },
-        "ursa-thinkcentre-m75q": {
-            "info": "Supported benchmark langs: C++, Java",
-            "default_filters": {
-                "arrow-commit": {
-                    "langs": {
-                        "C++": {"names": ["cpp-micro"]},
-                        "Java": {"names": ["java-micro"]},
-                    }
-                }
-            },
-            "supported_filters": ["lang", "command"],
-            "offline_warning_enabled": True,
-            "publish_benchmark_results": True,
-            "max_builds": 1,
-        },
+        # "ursa-thinkcentre-m75q": {
+        #     "info": "Supported benchmark langs: C++, Java",
+        #     "default_filters": {
+        #         "arrow-commit": {
+        #             "langs": {
+        #                 "C++": {"names": ["cpp-micro"]},
+        #                 "Java": {"names": ["java-micro"]},
+        #             }
+        #         }
+        #     },
+        #     "supported_filters": ["lang", "command"],
+        #     "offline_warning_enabled": True,
+        #     "publish_benchmark_results": True,
+        #     "max_builds": 1,
+        # },
         "ec2-t3-xlarge-us-east-2": {
             "info": "Supported benchmark langs: Python, R. Runs only benchmarks with cloud = True",
             "default_filters": {
