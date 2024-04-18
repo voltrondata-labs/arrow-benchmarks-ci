71249d2ef53d8226c2d62003650fa6c81dbdd658
Sam Albers <sam.albers@gmail.com>
2024-04-18 08:04:58 -0700

    Update config.py
    Co-authored-by: Austin Dickey <austin3spammy@gmail.com>

--- config.py
+++ config.py
@@ -201,7 +201,7 @@ class Config:
             "offline_warning_enabled": False,
             "publish_benchmark_results": True,
             "max_builds": 1,
-            "build_timeout": 270,
+            "build_timeout": 180,
         },
         "test-mac-arm": {
             "info": "Supported benchmark langs: C++, Python, R",
